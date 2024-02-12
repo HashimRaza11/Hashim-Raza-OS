59d12ccc82d8f461bb1f58e75058e5b1b3ebf258
Hashim Raza <108229556+HashimRaza11@users.noreply.github.com>
2024-02-12 21:55:45 +0530

    Add files via upload

---
+++ components/SEO/Meta.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Head from 'next/head';
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Hashim-UbuntuOs - CSE</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Hashim-UbuntuOs - CSE" />
+            <meta name="description"
+                content="Hashim Raza (Hashimraza11) Personal HashimOs Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Hashim Raza  (Hashimraza11)" />
+            <meta name="keywords"
+                content="Hashimraza11, Hashimraza11's HashimOs, Hashimraza11 linux, ubuntu HashimOs, Hashim Raza  protfolio,Hashim Raza  computer, Hashim Raza , Hashim Raza  OS, Hashim Raza  ubuntu HashimOs" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Hashim-UbuntuOs - CSE" />
+            <meta itemProp="description"
+                content="Hashim Raza (Hashimraza11) Personal HashimOs Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Hashim-UbuntuOs - CSE" />
+            <meta name="twitter:description"
+                content="Hashim Raza (Hashimraza11) Personal HashimOs Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="Hashimraza11" />
+            <meta name="twitter:creator" content="Hashimraza11" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Hashim-UbuntuOs - CSE" />
+            <meta name="og:description"
+                content="Hashim Raza (Hashimraza11) Personal HashimOs Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="https://github.com/HashimRaza11/" />
+            <meta name="og:site_name" content="Hashim Raza OS" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Ubuntu from "../components/ubuntu";
+import Meta from "../components/SEO/Meta";
+
+function App() {
+  return (
+    <>
+      <Meta />
+      <Ubuntu />
+    </>
+  )
+}
+
+export default App;
